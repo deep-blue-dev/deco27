42baf966ed396452ccf998a514d6097b1bbc62d9
hg16 <hmgaar@gmail.com>
2016-02-09 02:16:33 -0500

    project created, Products Categories crud done

---
+++ db/seeds.rb
@@ -0,0 +1,61 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+categories = [
+  {
+    name:"",
+    description:"",
+    image:""
+  },
+  {
+    name:"",
+    description:"",
+    image:""
+  },
+  {
+    name:"",
+    description:"",
+    image:""
+  },
+  {
+    name:"",
+    description:"",
+    image:""
+  },
+  {
+    name:"",
+    description:"",
+    image:""
+  },
+  {
+    name:"",
+    description:"",
+    image:""
+  },
+  {
+    name:"",
+    description:"",
+    image:""
+  },
+  {
+    name:"",
+    description:"",
+    image:""
+  }
+]
+
+products = [
+  {
+    name:"",
+    # price:0,
+    description:"",
+    image: "",
+    logo:"",
+    category_id:"",
+    mod_name:""
+  }
+]
